3ae2606d18434bbc2fc14c0de863925302b87593
Mirembe Peace Mercy <peace2de@gmail.com>
2025-09-08 16:06:36 +0300

    changed the file path

--- src/components/ProjectsPanel.tsx
+++ src/components/ProjectsPanel.tsx
@@ -4,30 +4,14 @@ function ProjectsPanel(){
             <p className="section-title text-white">Projects</p>
             <div className="card-container">
                 <div className="project-cards p-10 bg-info bg-opacity-10 border border-secondary rounded g-col-12">
-                    <img src="./public/typing_game.png" className="project-image img-fluid rounded" alt="project image"></img>
+                    <img src="/typing_game.png" className="project-image img-fluid rounded" alt="project image"></img>
                     <div className="project-description">
                         <h1 className="project-title text-primary">Typing Game</h1>
                         <p className="project-info">This game helps beginner developers learn how to type faster</p>
                     </div>
                 </div>
                 <div className="project-cards p-10 bg-info bg-opacity-10 border border-secondary rounded g-col-12">
-                    <img src="./public/typing_game.png" className="project-image img-fluid rounded" alt="project image"></img>
-                    <div className="project-description">
-                        <h1 className="project-title text-primary">Typing Game</h1>
-                        <p className="project-info">This game helps beginner developers learn how to type faster</p>
-                    </div>
-                </div>
-            </div>
-            <div className="card-container">
-                <div className="project-cards p-10 bg-info bg-opacity-10 border border-secondary rounded g-col-12">
-                    <img src="./public/typing_game.png" className="project-image img-fluid rounded" alt="project image"></img>
-                    <div className="project-description">
-                        <h1 className="project-title text-primary">Typing Game</h1>
-                        <p className="project-info">This game helps beginner developers learn how to type faster</p>
-                    </div>
-                </div>
-                <div className="project-cards p-10 bg-info bg-opacity-10 border border-secondary rounded g-col-12">
-                    <img src="./public/typing_game.png" className="project-image img-fluid rounded" alt="project image"></img>
+                    <img src="/typing_game.png" className="project-image img-fluid rounded" alt="project image"></img>
                     <div className="project-description">
                         <h1 className="project-title text-primary">Typing Game</h1>
                         <p className="project-info">This game helps beginner developers learn how to type faster</p>
@@ -36,14 +20,30 @@ function ProjectsPanel(){
             </div>
             <div className="card-container">
                 <div className="project-cards p-10 bg-info bg-opacity-10 border border-secondary rounded g-col-12">
-                    <img src="./public/typing_game.png" className="project-image img-fluid rounded" alt="project image"></img>
+                    <img src="/typing_game.png" className="project-image img-fluid rounded" alt="project image"></img>
                     <div className="project-description">
                         <h1 className="project-title text-primary">Typing Game</h1>
                         <p className="project-info">This game helps beginner developers learn how to type faster</p>
                     </div>
                 </div>
                 <div className="project-cards p-10 bg-info bg-opacity-10 border border-secondary rounded g-col-12">
-                    <img src="./public/typing_game.png" className="project-image img-fluid rounded" alt="project image"></img>
+                    <img src="/typing_game.png" className="project-image img-fluid rounded" alt="project image"></img>
+                    <div className="project-description">
+                        <h1 className="project-title text-primary">Typing Game</h1>
+                        <p className="project-info">This game helps beginner developers learn how to type faster</p>
+                    </div>
+                </div>
+            </div>
+            <div className="card-container">
+                <div className="project-cards p-10 bg-info bg-opacity-10 border border-secondary rounded g-col-12">
+                    <img src="/typing_game.png" className="project-image img-fluid rounded" alt="project image"></img>
+                    <div className="project-description">
+                        <h1 className="project-title text-primary">Typing Game</h1>
+                        <p className="project-info">This game helps beginner developers learn how to type faster</p>
+                    </div>
+                </div>
+                <div className="project-cards p-10 bg-info bg-opacity-10 border border-secondary rounded g-col-12">
+                    <img src="/typing_game.png" className="project-image img-fluid rounded" alt="project image"></img>
                     <div className="project-description">
                         <h1 className="project-title text-primary">Typing Game</h1>
                         <p className="project-info">This game helps beginner developers learn how to type faster</p>
