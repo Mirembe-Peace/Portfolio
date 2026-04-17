245b84f6e507f0433fc552b1c110002c73d8a02c
Mirembe Peace Mercy <peace2de@gmail.com>
2026-04-17 10:32:19 +0300

    Updated about me panel

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -3,7 +3,7 @@ function AboutMe(){
         <div className="about-me p-10  border border-secondary rounded">
             <h1 className="introduction">Hi there 👋, my name is Peace Mercy, CS student at Uganda Christian University</h1>
             <hr></hr>
-            <p className="about-me-phrase">I’m a Computer Science student passionate about exploring the intersection of IoT, embedded systems, and full-stack development. I enjoy creating solutions that connect hardware and software, from building smart cooling systems and LED scoreboards to developing interactive web applications like e-commerce platforms and typing games. With skills in Python, React, FastAPI, and embedded hardware (Arduino/ESP32), I focus on turning ideas into real, practical solutions that make life smarter, simpler, and more connected.</p>
+            <p className="about-me-phrase">Computer scientist specialising in embedded systems, robotics, AR/VR, and full-stack development, with a focus on solving real-world challenges in energy, digital access, and immersive experiences. Passionate about putting Uganda at the frontier of innovation through hands-on prototypes, open research, and community-driven technical education.</p>
         </div>
     );
 }
