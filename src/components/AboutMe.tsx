c6b00ec63e67d875cc66474089db46f0c4ee215b
Mirembe Peace Mercy <peace2de@gmail.com>
2026-04-17 10:33:15 +0300

    updated about me section

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,7 +1,7 @@
 function AboutMe(){
     return (
         <div className="about-me p-10  border border-secondary rounded">
-            <h1 className="introduction">Hi there 👋, my name is Peace Mercy, CS student at Uganda Christian University</h1>
+            <h1 className="introduction">Hi there 👋, my name is Peace Mercy, computer science student at Uganda Christian University</h1>
             <hr></hr>
             <p className="about-me-phrase">Computer scientist specialising in embedded systems, robotics, AR/VR, and full-stack development, with a focus on solving real-world challenges in energy, digital access, and immersive experiences. Passionate about putting Uganda at the frontier of innovation through hands-on prototypes, open research, and community-driven technical education.</p>
         </div>
